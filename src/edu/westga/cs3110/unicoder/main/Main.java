5729745ff013a91f4dbdb8301a9b1e38c2c9c7b1
Zachary Madden <zbmadden@protonmail.com>
2021-09-16 22:28:18 -0400

    Adding Initial Files

---
+++ src/edu/westga/cs3110/unicoder/main/Main.java
@@ -0,0 +1,10 @@
+package edu.westga.cs3110.unicoder.main;
+
+import java.io.Console;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -0,0 +1,15 @@
+package edu.westga.cs3110.unicoder.model;
+
+/**
+ * Codepoint class
+ * @version: Fall 2021
+ * @author zmadden
+ */
+public class Codepoint {
+
+    private String hexString;
+
+    public Codepoint(String hexString){
+        this.hexString = hexString;
+    }
+}
